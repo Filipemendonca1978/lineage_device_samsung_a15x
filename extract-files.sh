2ee90c65d016bf244db23472d41ba67f244a3359
Flopster101 <nahuelgomez329@gmail.com>
2025-08-14 17:26:04 -0300

    a15x: Patch some blobs to fix dependencies on deprecated HALs
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,26 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib*/android.hardware.bluetooth.audio-impl.so)
+            echo "Patching ${1} for BT audio AIDL version"
+            "${PATCHELF}" --replace-needed "android.hardware.bluetooth.audio-V2-ndk.so" "android.hardware.bluetooth.audio-V5-ndk.so" "${2}"
+            ;;
+
+        vendor/lib*/vendor.mediatek.hardware.pq_aidl-V1-ndk.so)
+            echo "Patching ${1} for graphics.common AIDL version"
+            "${PATCHELF}" --replace-needed "android.hardware.graphics.common-V3-ndk.so" "android.hardware.graphics.common-V6-ndk.so" "${2}"
+            ;;
+
+        vendor/lib*/*codec2_vpp_A*)
+            echo "Found VPP plugin: ${1}. Patching dependencies..."
+            "${PATCHELF}" --replace-needed "android.hardware.graphics.common-V3-ndk.so" "android.hardware.graphics.common-V6-ndk.so" "${2}"
+            "${PATCHELF}" --replace-needed "android.hardware.graphics.allocator-V1-ndk.so" "android.hardware.graphics.allocator-V2-ndk.so" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
